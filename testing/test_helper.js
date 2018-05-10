69fcfce41b9243b349560e3c21543fa6e1943d84
Ashish Singh <ashishcodes4@gmail.com>
2018-05-10 05:59:48 +0530

    adding new files

--- testing/test_helper.js
+++ testing/test_helper.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect 
     .once('open', () => console.log('Good to go!!!'))
-    .on('Error', (error) => {
-        console.warn('Error', error)
+    .on('error', (error) => {
+        console.warn('Warning', error)
     });
 
 
